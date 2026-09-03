1c959331148892b9a6392abc476ea2df14f995ee
Saiteja557 <gurralasaiteja123@gmail.com>
2026-09-03 12:12:40 +0530

    Create train_binary.py

---
+++ train_binary.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import layers, models
+
+IMG_SIZE = (128, 128)
+BATCH_SIZE = 32
+
+datagen = ImageDataGenerator(
+    rescale=1./255,
+    validation_split=0.2
+)
+
+train = datagen.flow_from_directory(
+    "binary_dataset",
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode="binary",
+    subset="training"
+)
+
+val = datagen.flow_from_directory(
+    "binary_dataset",
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode="binary",
+    subset="validation"
+)
+
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu', input_shape=(128,128,3)),
+    layers.MaxPooling2D(2,2),
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D(2,2),
+    layers.Conv2D(128, (3,3), activation='relu'),
+    layers.MaxPooling2D(2,2),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+model.fit(train, validation_data=val, epochs=10)
+
+model.save("models/dragonfruit_binary_model.h5")
